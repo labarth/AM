6f3e7c538d6facafbbc3bb714ad293560483eec1
alexander matiugin <alexander.matiugin@pandadoc.com>
2020-12-01 17:19:40 +0300

    add signUp

--- src/modules/Home/Home.js
+++ src/modules/Home/Home.js
@@ -1,1 +1,9 @@
-export const Home = () => <div>HOME PAGE!</div>;
+import { apiService } from '../Main/api';
+
+export const Home = () => {
+  const getUsers = async () => {
+    const { data: users } = await apiService.get('api/user');
+    console.log(users);
+  };
+  return (<div><button onClick={getUsers}>get users</button>HOME PAGE!</div>);
+}
